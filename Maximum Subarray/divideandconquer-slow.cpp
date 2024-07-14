7bfc46ce8be6baa8ae9572ea8002ef86981a56ab
ruben <79235229+Streakwind@users.noreply.github.com>
2024-07-14 16:05:08 -0700

    Update divideandconquer-slow.cpp
    add description to why line 50 works

--- Maximum Subarray/divideandconquer-slow.cpp
+++ Maximum Subarray/divideandconquer-slow.cpp
@@ -34,6 +34,8 @@ ll crossingsub (vector<ll> theList, ll low, ll mid, ll high) {
 ll maxsubarray (vector<ll> theList, ll low, ll high) {
     ll mid = 0;
 
+    //cout << low << ", " << high << endl;
+
     if (low > high) {
         return INT64_MIN; //invalid
     }
@@ -46,6 +48,8 @@ ll maxsubarray (vector<ll> theList, ll low, ll high) {
     mid /= 2;
 
     return max(max(maxsubarray(theList, low, mid), maxsubarray(theList, mid+1, high)), crossingsub(theList, low, mid, high)); //do not put high+1 for highest point because then it keeps adding 1. only mid can be modified as it is calculated every time
+    //mid+1 needed on right side or else mid always will be less than high
+    //mid or mid-1 on left doesn't matter, 7 or 7-1 = 6 divided by two result in the same number
 }
 
 int main () {
